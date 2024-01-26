9ccc5a9c334a8a9a227c16887425d2bfe7a9d106
Jason Mansfield <jmansfield@gmail.com>
2024-01-26 13:33:46 -0500

    copy png debugging for iOS

--- src/components/charts/ShareableChart.js
+++ src/components/charts/ShareableChart.js
@@ -15,47 +15,18 @@ export const ShareableChart = ({type, fileNamePrefix, title, data,
     const [interval, setInterval] = useState(null);
     const sharedCardRef = useRef();
 
-    const copyToClipboard = async function(pngBlob) {
-        if(typeof navigator === "undefined" || typeof window === "undefined")
-        {
-            return;
-        }
-
-        try {
-            await navigator.clipboard.write([
-                new window.ClipboardItem({
-                    [pngBlob.type]: pngBlob
-                })
-            ]);
-            alert("Copied to clipboard. Paste on X to share.")
-        } catch (error) {
-            alert("Unable to copy to clipboard: " + error)
-        }
-    }
-
     function dataURItoBlob(dataURI) {
-        // convert base64 to raw binary data held in a string
-        // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
         let byteString = atob(dataURI.split(',')[1]);
-
-        // separate out the mime component
         let mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-
-        // write the bytes of the string to an ArrayBuffer
         let ab = new ArrayBuffer(byteString.length);
-
-        // create a view into the buffer
         let ia = new Uint8Array(ab);
 
-        // set the bytes of the buffer to the correct values
         for (let i = 0; i < byteString.length; i++) {
             ia[i] = byteString.charCodeAt(i);
         }
 
-        // write the ArrayBuffer to a blob, and you're done
         let blob = new Blob([ab], {type: mimeString});
         return blob;
-
     }
 
     const sleep = ms => new Promise(r => setTimeout(r, ms));
