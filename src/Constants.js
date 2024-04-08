3e44f3e34c17778a17be6ed7acfda52d306f2b93
Jason Mansfield <jmansfield@gmail.com>
2024-04-08 13:00:08 -0400

    fee collection, all time stats

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-//export const GRAPHQL_ENDPOINT="https://2ho7i6mowc.execute-api.us-east-1.amazonaws.com/pdexanalytics";
-export const GRAPHQL_ENDPOINT="http://localhost:4000/graphql";
+export const GRAPHQL_ENDPOINT="https://2ho7i6mowc.execute-api.us-east-1.amazonaws.com/pdexanalytics";
+//export const GRAPHQL_ENDPOINT="http://localhost:4000/graphql";
 
