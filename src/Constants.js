ce7b12ff41fd97cb65073895654a8f6d263bd9fa
Jason Mansfield <jmansfield@gmail.com>
2024-06-08 14:49:00 -0400

    update graphql endpoint

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,3 @@
-export const GRAPHQL_ENDPOINT="https://2ho7i6mowc.execute-api.us-east-1.amazonaws.com/pdexanalytics";
+export const GRAPHQL_ENDPOINT="https://d8wkh6f9e0gbh.cloudfront.net/pdexanalytics";
 //export const GRAPHQL_ENDPOINT="http://localhost:4000/graphql";
 
